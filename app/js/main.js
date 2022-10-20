fd688578f08bcd520b05d7785295a245206656db
VladSiy <webtouch.com.ua@gmail.com>
2022-10-20 14:56:25 +0300

    Changed mobile menu to js

--- app/js/main.js
+++ app/js/main.js
@@ -58,3 +58,39 @@ $(function(){
 		player.addEventListener('click', loadPlayer, config);
 	})
 
+	/*
+	 * Dropdown menu mobile
+	 */
+
+	var menu_mobile_btn = document.getElementById("menu_mobile_btn");
+	var menu_mobile = document.querySelector(".menu");
+	var submenus = document.querySelectorAll(".menu__list-submenu-list");
+	var arrows_down = document.querySelectorAll(".arrow_down");
+
+	// Toggle menu on button click 
+	menu_mobile_btn.addEventListener('click', function(){
+		menu_mobile.classList.toggle('menu__active');
+
+		// Collapse all sub menus on button click 
+		submenus.forEach(function(item){
+			item.classList.remove('menu__active');
+		});
+	});
+
+	// Toggle sub menu on arrow click 
+	arrows_down.forEach(function(item){
+		item.addEventListener('click', function(){
+			this.closest('.menu__list-item-has-children').querySelector('.menu__list-submenu-list').classList.toggle('menu__active');
+		});
+	});
+
+	// Collapse menu if the user clicks outside of it
+	window.onclick = function(event){
+		if(!event.target.matches('#menu_mobile_btn, .menu__list-item, .arrow_down, .search__form-field')){
+			if(menu_mobile.classList.contains('menu__active')){
+				menu_mobile.classList.remove('menu__active');
+			}
+		}
+	}
+
+});
